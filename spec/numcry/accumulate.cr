55287ae8fab2ee00d89f65e05d5df2fb8e424b02
Kim Burgess <kim@acaprojects.com>
2019-08-03 01:43:16 +1000

    neaten up accumulate specs

--- spec/numcry/accumulate.cr
+++ spec/numcry/accumulate.cr
@@ -1,14 +1,19 @@
 require "./../spec_helper"
 
 describe Numcry::Accumulate do
-  it { accumulate((1..3), 10) { |a, b| a + b }.should eq([11, 13, 16]) }
-  it { accumulate((1..3), 0) { |a, b| a - b }.should eq([-1, -3, -6]) }
-  it { cumsum((1..3)).should eq([1, 3, 6]) }
-  it { cumprod((1..3)).should eq([1, 2, 6]) }
+  describe ".accumulate" do
+    it { accumulate((1..3), 10) { |a, b| a + b }.should eq([11, 13, 16]) }
+    it { accumulate((1..3), 0) { |a, b| a - b }.should eq([-1, -3, -6]) }
+  end
 
-  it "handles empty collections" do
-    cumsum([] of Int32).should eq([] of Int32)
-    cumprod([] of Int32).should eq([] of Int32)
+  describe ".cumsum" do
+    it { cumsum((1..3)).should eq([1, 3, 6]) }
+    it { cumsum([] of Int32).should eq([] of Int32) }
+  end
+
+  describe ".cumprod" do
+    it { cumprod((1..3)).should eq([1, 2, 6]) }
+    it { cumprod([] of Int32).should eq([] of Int32) }
   end
 end
 
