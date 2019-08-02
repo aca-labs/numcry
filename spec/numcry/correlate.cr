a950853843cbdf826387dfeb8c33f60ecdb79738
Kim Burgess <kim@acaprojects.com>
2019-08-03 02:20:41 +1000

    implement pearson correlation

--- spec/numcry/correlate.cr
+++ spec/numcry/correlate.cr
@@ -8,6 +8,12 @@ describe Numcry::Stats do
     it { cov(a, b, ddof: 1).should be_close(-7.500, 1e-3) }
     it { cov(a, b).should eq(cov b, a) }
   end
+
+  describe ".corrcoef" do
+    a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    b = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+    it { corrcoef(a, b).should be_close(-1, 1e-10) }
+  end
 end
 
 describe Enumerable do
@@ -21,5 +27,17 @@ describe Enumerable do
     it { b.cov(b, ddof: 1).should be_close(2.333, 1e-3) }
     it { b.cov(c, ddof: 1).should be_close(1.500, 1e-3) }
     it { c.cov(c, ddof: 1).should be_close(1.000, 1e-3) }
+  end
+
+  describe "#corrcoef" do
+    a = [1, 2, 3, 4, 6, 7, 8, 9]
+    b = [2, 4, 6, 8, 10, 12, 13, 15]
+    c = [-1, -2, -2, -3, -4, -6, -7, -8]
+    d = a.map { |x| -1 * x }
+    it { a.corrcoef(a).should eq(1) }
+    it { a.corrcoef(b).should be_close(0.99535001, 1e-8) }
+    it { a.corrcoef(c).should be_close(-0.9805214, 1e-8) }
+    it { b.corrcoef(c).should be_close(-0.97172394, 1e-8) }
+    it { a.corrcoef(d).should eq(-1) }
   end
 end
--- src/numcry/correlate.cr
+++ src/numcry/correlate.cr
@@ -1,3 +1,5 @@
+require "./stats"
+
 # Methods for computing correlation coefficients between `Enumerable`
 # collections.
 module Numcry::Correlate
@@ -9,10 +11,19 @@ module Numcry::Correlate
     e = a.mean * b.mean
     a.zip(b).sum { |x, y| x * y - e } / (a.size - ddof)
   end
+
+  # Compute the Pearson correlation coefficient for *a* and *b*.
+  def corrcoef(a : Enumerable, b : Enumerable, ddof = 0.0)
+    cov(a, b, ddof) / (std(a, ddof) * std(b, ddof))
+  end
 end
 
 module Enumerable(T)
   def cov(other : Enumerable, ddof = 0.0)
     Numcry::Correlate.cov self, other, ddof
+  end
+
+  def corrcoef(other : Enumerable, ddof = 0.0)
+    Numcry::Correlate.corrcoef self, other, ddof
   end
 end
